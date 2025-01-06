449394071ebf3e58f67c923e8e33b3dec15973df
Puttara K S <puttaraju.ks@hashicorp.com>
2025-01-06 22:53:37 +0530

    AWS provider added

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.82.2"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
 provider "aws" {
   region = var.region
 }
