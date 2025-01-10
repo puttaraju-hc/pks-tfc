ce48899de306efd5e3a83168f486654693246e6e
Puttara K S <puttaraju.ks@hashicorp.com>
2025-01-10 15:05:48 +0530

    main.tf changes

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
-provider "aws" {
-  region = "us-west-2"
+terraform {
+  cloud {
+  
+    organization = "pks-hc"
+    
+    workspaces {
+      name = "demo"
+    }
+  
+  }
 }
-
-resource "aws_apigatewayv2_api" "example" {
-  name                       = "example-websocket-api"
-  protocol_type              = "WEBSOCKET"
-  route_selection_expression = "$request.body.action"
-}
-
-resource "aws_apigatewayv2_stage" "example" {
-  api_id = aws_apigatewayv2_api.example.id
-  name   = "example-stage"
-}
-
